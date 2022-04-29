5eb4b4ca45805da3cef96b745eea2176002f8880
Harshit Hajela <hhajela@splunk.com>
2022-04-29 12:47:33 -0600

    initial commit

---
+++ src/main/java/example/micronaut/FooConfig.java
@@ -0,0 +1,29 @@
+package example.micronaut;
+
+import io.micronaut.context.annotation.ConfigurationInject;
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.convert.format.MapFormat;
+import io.micronaut.core.naming.conventions.StringConvention;
+
+import java.util.Map;
+
+@ConfigurationProperties("foo.config")
+public class FooConfig {
+  private final String fooBar;
+  private final Map<String, String> barBlah;
+
+  @ConfigurationInject
+  public FooConfig(String fooBar,
+    @MapFormat(transformation = MapFormat.MapTransformation.FLAT) Map<String, String> barBlah) {
+    this.fooBar = fooBar;
+    this.barBlah = barBlah;
+  }
+
+  public String getFooBar() {
+    return fooBar;
+  }
+
+  public Map<String, String> getBarBlah() {
+    return barBlah;
+  }
+}
